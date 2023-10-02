abad24a67252457c0b01eae5c01ddcd88936e3da
scher470 <120848089+nxpinhum5326@users.noreply.github.com>
2023-10-02 23:42:57 +0300

    Create Troll.php

---
+++ src/scher/trollxd/troll/Troll.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace scher\trollxd\troll;
+
+use pocketmine\player\Player;
+use pocketmine\scheduler\Task;
+
+abstract class Troll {
+    
+    abstract public function action(Player $trolled): void;
+
+}
