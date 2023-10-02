7b1a2d2746c43aabd89f23d4e32b4987233fc056
scher470 <120848089+nxpinhum5326@users.noreply.github.com>
2023-10-02 23:44:42 +0300

    Update Manager.php

--- src/scher/trollxd/managers/Manager.php
+++ src/scher/trollxd/managers/Manager.php
@@ -5,11 +5,10 @@ namespace scher\trollxd\managers;
 use scher\trollxd\Loader;
 
 class Manager {
-	
 	public function getAntiCheatPrefix(): string {
 		return str_replace("&", "§", Loader::getInstance()->getConfig()->get("anti-cheat-prefix"));
 	}
-
+	
 	public function getAntiCheatMessage(): string {
 		return $this->getAntiCheatPrefix() . str_replace("&", "§", Loader::getInstance()->getConfig()->get("anti-cheat-message"));
 	}
